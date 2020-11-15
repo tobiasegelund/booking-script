e339eaf603dc284a880726f22e12a6122afac53b
Tobias Egelund <teunited.1994@gmail.com>
2020-11-15 15:29:04 +0100

    v1.1

--- ground.py
+++ ground.py
@@ -1,54 +1,50 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import Select, WebDriverWait
 import schedule
 from credentials import username, password
 
 
-usr = username # Indsæt dit eget username
-pwd = password # Insæt dit eget password
+usr = username  # Insert your own username
+pwd = password  # Insert your own password
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+
 def job():
     driver.get('https://booking.sport-solutions.dk/login/da-DK?issue=false')
-    print ("Opened booking-site")
+    print("Opened booking site")
     sleep(1)
 
     username_box = driver.find_element_by_id('username')
     username_box.send_keys(usr)
-    print ("Email Id entered")
     sleep(1)
 
     password_box = driver.find_element_by_id('password')
     password_box.send_keys(pwd)
-    print ("Password entered")
     sleep(1)
 
     login_box = driver.find_element_by_xpath("//form[1]/button[1]")
     login_box.click()
-    sleep(3)
+    print("Logged in")
+    sleep(5)
 
     try:
         driver.get('https://booking.sport-solutions.dk/main/weekCalendar')
-        sleep(3)
+        sleep(5)
 
     except:
-        pass
-    try:
-        driver.get('https://booking.sport-solutions.dk/main/weekCalendar')
-        sleep(3)
-    except:
-        pass
+        print('Could not find Calendar')
 
     try:
         arrow_move = driver.find_element_by_xpath("//div[@class='fc-toolbar']/div[@class='fc-right']/div[@class='fc-button-group'][2]")
         arrow_move.click()
-        sleep(3)
+        sleep(5)
         arrow_move.click()
-        sleep(3)
+        sleep(5)
 
         # # ONLY WHEN BOOKING ON MONDAY
         # arrow_move.click()
@@ -56,40 +52,35 @@ def job():
 
         type_box = Select(driver.find_element_by_xpath("//div[@id='selector_type']/select[1]"))
         type_box.select_by_index(1)
-        sleep(1)
+        sleep(2)
 
     except:
         pass
 
 
 def booking_hour(hour):
-    try:
-        padel_table = driver.find_element_by_xpath(f"//div[@class='fc-view-container']/div[@class='fc-view fc-basicWeek-view fc-basic-view']/table/tbody[@class='fc-body']/tr/td/div[@class='fc-day-grid-container']/div[@class='fc-day-grid']/div[@class='fc-row fc-week fc-widget-content']/div[@class='fc-content-skeleton']/table/tbody/tr[{hour}]/td[1]")
-        padel_table.click()
-        sleep(1)
-    except:
-        pass
+    padel_table = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, f"//div[@class='fc-view-container']/div[@class='fc-view fc-basicWeek-view fc-basic-view']/table/tbody[@class='fc-body']/tr/td/div[@class='fc-day-grid-container']/div[@class='fc-day-grid']/div[@class='fc-row fc-week fc-widget-content']/div[@class='fc-content-skeleton']/table/tbody/tr[{hour}]/td[1]")))
+    padel_table.click()
 
     try:
-        tilmeld = driver.find_element_by_xpath("//div[@class='modal-dialog']/div[@class='modal-content']/div[@class='modal-footer ng-scope']/button[@ng-click='bookEvent()']")
+        tilmeld = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='modal-dialog']/div[@class='modal-content']/div[@class='modal-footer ng-scope']/button[@ng-click='bookEvent()']")))
         tilmeld.click()
-    except:
-        pass
 
-    try:
-        cancel = driver.find_element_by_xpath("//div[@class='modal-dialog']/div[@class='modal-content']/div[@class='modal-footer ng-scope']/button[@ng-click='cancel()']")
+        print("Booked!")
+        luk = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[3]/div/div/div[3]/button")))
+        luk.click()
+
+    except:
+        print("Could not find booking button")
+
+        cancel = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='modal-dialog']/div[@class='modal-content']/div[@class='modal-footer ng-scope']/button[@ng-click='cancel()']")))
+
         cancel.click()
 
-    except:
-        pass
-
-    sleep(1)
 
 def book():
-
     kategori_box = Select(driver.find_element_by_xpath("//select[@ng-model='selectedCategory']"))
     kategori_box.select_by_index(3)
-    sleep(2)
 
     booking_hour(13)
     booking_hour(14)
@@ -97,6 +88,7 @@ def book():
 
     driver.quit()
     print("Finished")
+
 
 schedule.every().day.at("23:59").do(job)
 schedule.every().day.at("00:00").do(book)
